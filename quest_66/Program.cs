922b55db65e1b348b6420b66b68f44838bab7a0d
GrigaTan <olimp.240@gmail.com>
2024-04-15 00:54:48 +0300

    gotovo8

--- quest_66/Program.cs
+++ quest_66/Program.cs
@@ -2,10 +2,11 @@
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение N: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int summa(int m, int n)
+int summ(int m, int n)
 {
 if (m==n)
 return n;
-else return m + summa(m + 1, n);
+else return m + summ(m + 1, n);
 }
-Console.Write($"Сумма натуральных элементов в промежутке от M до N равно {summa(m,n)}");
+Console.Write($"Сумма натуральных элементов в промежутке от M до N равно {summ(m,n)}");
+
